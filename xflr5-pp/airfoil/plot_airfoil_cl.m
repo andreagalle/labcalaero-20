b776f0ef239021f3da64fefe307410d6d159c79a
andreagalle <andreagalle@hotmail.it>
2020-03-24 15:16:39 +0100

    Cl & Cd calculation - integrating the Cp over the airfoil

--- xflr5-pp/airfoil/plot_airfoil_cl.m
+++ xflr5-pp/airfoil/plot_airfoil_cl.m
@@ -1,4 +1,6 @@
 
+clear
+
 filename       = 'data/NACA 0012.dat';
 delimiterIn    = ' ';
 headerlinesIn  = 1;
@@ -11,14 +13,33 @@ coord  = char(fields(1));
 
 airfoil_coord = airfoil_struct.(coord);
 
-x = airfoil_coord(:,1); 
-y = airfoil_coord(:,2); 
+x = airfoil_coord(:,1); % airfoil coordinates 
+y = airfoil_coord(:,2); % airfoil coordinates  
 
-%figure(1);
-%plot(airfoil_coord(:,1),airfoil_coord(:,2),'.','MarkerSize',20);
-%
-%axis equal;
-%hold off;
+npoints = length(x)
+
+aoa = 4.0; % deg
+
+aoa = aoa*3.14/180; % rad
+
+% Specify the coordinates of the center of rotation
+
+Xc = 0.25;
+Yc = 0.00;
+
+X = x; 
+Y = y; 
+
+% Rotating the airfoil about the 1/4 chord point 
+
+x =   (X - Xc)*cos(aoa) + (Y - Yc)*sin(aoa) + Xc;
+y = - (X - Xc)*sin(aoa) + (Y - Yc)*cos(aoa) + Yc;
+
+figure(1);
+plot(x,y,'.','MarkerSize',20);
+
+axis equal;
+hold off;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -34,25 +55,38 @@ analysis  = char(fields(1));
 
 airfoil_analysis = analysis_struct.(analysis);
 
-p = airfoil_analysis(:,2);
+p = airfoil_analysis(:,2); % just the pressure distribution
 
-%figure(2);
-%plot(airfoil_analysis(:,1),airfoil_analysis(:,2),'g.','MarkerSize',20);
-%
-%hold on
-%set(gca, 'YDir','reverse');
-%% axis equal;
-%hold off
+figure(2);
+plot(airfoil_analysis(:,1),airfoil_analysis(:,2),'g.','MarkerSize',20);
+
+hold on
+set(gca, 'YDir','reverse');
+% axis equal;
+hold off
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-for k = 1 : (length(x) - 1)
+npanels = npoints - 1
+
+for k = 1 : npanels 
   
-  l(k) = sqrt((x(k+1)-x(k))^2+(y(k+1)-y(k))^2);
-  t(k) = (x(k+1)-x(k))/l(k);
-  n(k) = t(k)*l(k);
-  a(k) = n(k)*p(k);
+  deltaX = x(k+1) - x(k); 
+  deltaY = y(k+1) - y(k);
+
+  Cpress = (p(k+1) + p(k))/2;
+
+  panlen = sqrt(deltaX^2 + deltaY^2);
+
+  ny = deltaX/panlen;
+  nx = deltaY/panlen;
+
+  lift(k) = ny*Cpress*panlen;
+  drag(k) = nx*Cpress*panlen;
 
 end
 
-cl = sum(a)
+cl = sum(lift)
+
+cd = sum(drag)
+
