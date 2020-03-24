ecf2acc2b11bebcc4dd0108a70c90e540fd84321
andreagalle <andreagalle@hotmail.it>
2020-03-24 15:50:17 +0100

    linear plot of Cl vs Alfa

--- xflr5-pp/airfoil/plot_airfoil_cl.m
+++ xflr5-pp/airfoil/plot_airfoil_cl.m
@@ -18,75 +18,75 @@ y = airfoil_coord(:,2); % airfoil coordinates
 
 npoints = length(x)
 
-aoa = 4.0; % deg
-
-aoa = aoa*3.14/180; % rad
+for aoa = 0:5 % deg
+  
+  aux = floor(aoa+1);
+  
+  filename       = strcat('data/naca12-cp-a',num2str(aoa),'0.txt');
+  
+  aoa = double(aoa)*3.14/180; % rad
 
 % Specify the coordinates of the center of rotation
 
-Xc = 0.25;
-Yc = 0.00;
+  Xc = 0.25;
+  Yc = 0.00;
 
-X = x; 
-Y = y; 
+  X = x; 
+  Y = y; 
 
 % Rotating the airfoil about the 1/4 chord point 
 
-x =   (X - Xc)*cos(aoa) + (Y - Yc)*sin(aoa) + Xc;
-y = - (X - Xc)*sin(aoa) + (Y - Yc)*cos(aoa) + Yc;
-
-figure(1);
-plot(x,y,'.','MarkerSize',20);
-
-axis equal;
-hold off;
+  x =   (X - Xc)*cos(aoa) + (Y - Yc)*sin(aoa) + Xc;
+  y = - (X - Xc)*sin(aoa) + (Y - Yc)*cos(aoa) + Yc;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-filename       = 'data/naca12-cp-a40.txt';
-delimiterIn    = ' ';
-headerlinesIn  = 6;
-format long;
-
-analysis_struct = importdata(filename,delimiterIn,headerlinesIn);
-
-fields = fieldnames(analysis_struct);
-analysis  = char(fields(1));
-
-airfoil_analysis = analysis_struct.(analysis);
-
-p = airfoil_analysis(:,2); % just the pressure distribution
-
-figure(2);
-plot(airfoil_analysis(:,1),airfoil_analysis(:,2),'g.','MarkerSize',20);
-
-hold on
-set(gca, 'YDir','reverse');
-% axis equal;
-hold off
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-npanels = npoints - 1
-
-for k = 1 : npanels 
+  delimiterIn    = ' ';
+  headerlinesIn  = 6;
+  format long;
   
-  deltaX = x(k+1) - x(k); 
-  deltaY = y(k+1) - y(k);
+  analysis_struct = importdata(filename,delimiterIn,headerlinesIn);
+  
+  fields = fieldnames(analysis_struct);
+  analysis  = char(fields(1));
+  
+  airfoil_analysis = analysis_struct.(analysis);
+  
+  p = airfoil_analysis(:,2); % just the pressure distribution
 
-  Cpress = (p(k+1) + p(k))/2;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-  panlen = sqrt(deltaX^2 + deltaY^2);
-
-  ny = deltaX/panlen;
-  nx = deltaY/panlen;
-
-  lift(k) = ny*Cpress*panlen;
-  drag(k) = nx*Cpress*panlen;
+  npanels = npoints - 1;
+  
+  for k = 1 : npanels 
+    
+    deltaX = x(k+1) - x(k); 
+    deltaY = y(k+1) - y(k);
+  
+    Cpress = (p(k+1) + p(k))/2;
+  
+    panlen = sqrt(deltaX^2 + deltaY^2);
+  
+    ny = deltaX/panlen;
+    nx = deltaY/panlen;
+  
+    lift(k) = ny*Cpress*panlen;
+    drag(k) = nx*Cpress*panlen;
+  
+  end
+  
+  cl(aux) = sum(lift);
+  cd(aux) = sum(drag);
+  
+  alfa(aux)=aux-1;
 
 end
 
-cl = sum(lift)
+display(npanels)
 
-cd = sum(drag)
+figure(3);
+plot(alfa(:),cl(:),'r-','MarkerSize',20);
+
+hold on
+axis equal;
 
